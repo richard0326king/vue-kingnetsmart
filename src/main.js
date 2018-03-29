6f2fae090d8dd5a5435aced56e5885d75b554a9d
wanyu0724 <wanyu0724@gmail.com>
2018-03-29 10:22:57 +0800

    jQuery + bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import jQuery from 'jquery'
+global.jQuery = jQuery
+let bootstrap = require('bootstrap')
 
 Vue.config.productionTip = false
 
@@ -10,6 +13,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
